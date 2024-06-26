400ef91231747dfc8180c9a8bca82e9f489d928c
Stanislav <s.tyulkov@gmail.com>
2024-06-26 10:49:56 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringtyulkovcw2/SkyproSpringTyulkovCw2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringtyulkovcw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringTyulkovCw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringTyulkovCw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringtyulkovcw2/SkyproSpringTyulkovCw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringtyulkovcw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringTyulkovCw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
